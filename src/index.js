87c040de050d68c091ba6cb953611830ba2524f0
Serg Zheleznikov <polwen@yandex.ru>
2020-08-04 16:47:15 +0300

    parallax first steps

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext;
 import 'lazysizes';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 import "lazysizes/plugins/unveilhooks/ls.unveilhooks";
+import Rellax from 'rellax';
 import simpleParallax from 'simple-parallax-js';
 
 import audioVizualization from './js/audioVizualization';
 import PlaySpaceSound from './js/PlaySpaceSound';
 import RotateCard from './js/RotateCard';
+import SimpleParallax from 'simple-parallax-js';
 
 const planetBlocks = document.querySelectorAll('.planet-block');
 const cards = document.querySelectorAll('.card');
@@ -22,14 +24,22 @@ cards.forEach(card => new RotateCard(card));
 // paralax-scrolling
 
 const planets = document.querySelectorAll('.planet');
-// console.log(planets)
+console.log(planets)
 
-new simpleParallax(planets, {
-  // scale: 2,
-  delay: 1.6,
-  transition: 'cubic-bezier(0,0,0,1)',
-  // maxTransition: 80
-});
+const parallax = new Rellax('.planet');
+
+// new SimpleParallax(document.querySelector('.mars__img'), {
+//   delay: 1.6,
+//   transition: 'cubic-bezier(0,0,0,1)',
+//   scale: 1.3
+// });
+
+// new simpleParallax(planets, {
+//   // scale: 1.5,
+//   delay: 1.6,
+//   transition: 'cubic-bezier(0,0,0,1)',
+//   // maxTransition: 80
+// });
 
 // const rellax = new Rellax('.rellax', {
 //   speed: 2,
