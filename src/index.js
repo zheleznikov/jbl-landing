f7cd65646b664d7c3d7359393a1376ac3ef05988
Serg Zheleznikov <polwen@yandex.ru>
2020-08-18 19:09:50 +0300

    add sharing

--- src/index.js
+++ src/index.js
@@ -67,8 +67,8 @@ function firstOpenPopupQuizHandle() {
 function quizHandle() {
   sessionStorage.setItem("number", +sessionStorage.getItem("number") + 1);
   let number = sessionStorage.getItem("number");
-  
-  console.log('количество правильных ответов - ',  sessionStorage.getItem("answer"))
+
+  console.log('количество правильных ответов - ', sessionStorage.getItem("answer"))
   console.log('пройденные вопросы - ', number)
 
   if (number > 8) {
@@ -80,6 +80,7 @@ function quizHandle() {
     popupContent.classList.add('popup__content_off');
     popupResult.classList.remove('result_off');
     new Result(popup, resData);
+    shareImageHandle();
 
   } else {
     quizBody.innerHTML = "";
@@ -92,13 +93,122 @@ function quizHandle() {
 
 
 
-openButtonPopup.addEventListener("click",firstOpenPopupQuizHandle);
-againButton.addEventListener("click",firstOpenPopupQuizHandle);
+openButtonPopup.addEventListener("click", firstOpenPopupQuizHandle);
+againButton.addEventListener("click", firstOpenPopupQuizHandle);
 nextButton.addEventListener("click", quizHandle);
 
 // testing
-// popupContent.classList.add('popup__content_off');
-// popupResult.classList.remove('result_off');
-// document.querySelector('.popup').classList.add('popup_on')
 
 
+
+
+const ogImage = document.querySelector('meta[property="og:image"]');
+const twitterImage = document.querySelector('meta[name="twitter:image"]');
+const vkImage = document.querySelector('meta[name="vk:image"]');
+
+const yaShare = document.querySelector('.ya-share2');
+
+yaShare.setAttribute('data-image', './images/res00.png');
+yaShare.setAttribute('data-image:vkontakte', './images/res00.png');
+yaShare.setAttribute('data-image:facebook', './images/res00.png');
+yaShare.setAttribute('data-image:whatsapp', './images/res00.png');
+yaShare.setAttribute('data-image:telegram', './images/res00.png');
+
+function setYaShare(link) {
+  yaShare.setAttribute('data-image', link);
+  yaShare.setAttribute('data-image:vkontakte', link);
+  yaShare.setAttribute('data-image:facebook', link);
+  yaShare.setAttribute('data-image:whatsapp', link);
+  yaShare.setAttribute('data-image:telegram', link);
+  console.log(yaShare);
+}
+
+
+const socialMetaArr = [ogImage, twitterImage, vkImage];
+
+
+function shareImageHandle() {
+  console.log('handleShare start');
+  const result = +sessionStorage.getItem("answer");
+
+  switch (result) {
+    case 0:
+      socialMetaArr.forEach(tag => tag.content = './images/res00.png');
+      setYaShare('./images/res00.png')
+      socialMetaArr.forEach(tag => console.log(tag));
+
+      break;
+    case 1:
+      socialMetaArr.forEach(tag => tag.content = './images/res01.png');
+      setYaShare('./images/res01.png')
+      socialMetaArr.forEach(tag => console.log(tag));
+      break;
+
+    case 2:
+
+      setYaShare('./images/res02.png')
+
+      socialMetaArr.forEach(tag => tag.content = './images/res02.png');
+      socialMetaArr.forEach(tag => console.log(tag));
+
+      break;
+    case 3:
+      setYaShare('./images/res03.png')
+
+      socialMetaArr.forEach(tag => tag.content = './images/res03.png');
+      socialMetaArr.forEach(tag => console.log(tag.content));
+
+      break;
+    case 4:
+      setYaShare('./images/res04.png')
+
+      socialMetaArr.forEach(tag => tag.content = './images/res04.png');
+      socialMetaArr.forEach(tag => console.log(tag));
+
+      break;
+    case 5:
+      setYaShare('./images/res05.png')
+
+      socialMetaArr.forEach(tag => tag.content = './images/res05.png');
+      socialMetaArr.forEach(tag => console.log(tag));
+
+      break;
+    case 6:
+      setYaShare('./images/res06.png')
+
+      socialMetaArr.forEach(tag => tag.content = './images/res06.png');
+      socialMetaArr.forEach(tag => console.log(tag));
+
+      break;
+    case 7:
+      setYaShare('./images/res07.png')
+
+      socialMetaArr.forEach(tag => tag.content = './images/res07.png');
+      socialMetaArr.forEach(tag => console.log(tag));
+
+      break;
+    case 8:
+      setYaShare('./images/res08.png')
+
+      socialMetaArr.forEach(tag => tag.content = './images/res08.png');
+      socialMetaArr.forEach(tag => console.log(tag));
+
+      break;
+    case 9:
+      setYaShare('./images/res09.png')
+
+      socialMetaArr.forEach(tag => tag.content = './images/res09.png');
+      socialMetaArr.forEach(tag => console.log(tag));
+
+      break;
+    case 10:
+      setYaShare('./images/res10.png')
+
+      socialMetaArr.forEach(tag => tag.content = './images/res10.png');
+      socialMetaArr.forEach(tag => console.log(tag));
+
+      break;
+
+  }
+
+}
